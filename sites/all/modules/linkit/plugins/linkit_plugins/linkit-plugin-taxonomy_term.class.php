661d77bedf637e7769b49ccd3ea61d0855e83666
Orestes Sanchez <orestes@acm.org>
2012-03-31 18:58:26 +0200

    Added Linkit module

---
+++ sites/all/modules/linkit/plugins/linkit_plugins/linkit-plugin-taxonomy_term.class.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Define Linkit term plugin class.
+ *
+ * This is empty as we just have to define that this class exists.
+ */
+
+class LinkitPluginTaxonomy_Term extends LinkitPluginEntity {}
